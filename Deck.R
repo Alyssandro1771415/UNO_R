9c8ca315b912eae5577cfc35ca18c3fc16043170
Alyssandro <dyogo.alyssandro@gmail.com>
2024-03-09 22:24:43 -0300

    fix(Correções Lógicas): Correções na lógica de embaralhamento

--- Deck.R
+++ Deck.R
@@ -1,25 +1,32 @@
 library(R6)
 
-Deck <- R6Class(
-  "Deck",
+Deck <- R6Class("Deck",
+  
   public = list(
     
     cards = NULL,
-    
+
     initialize = function() {
-      
-      self$cards <- rbind(
-        c("azul", 7),
-        c("vermelho", 11),
-        c("verde", 3),
-        c("amarelo", 8)
-      )
-      
-      self$cards <- sample(self$cards)
-      print(self$cards)
-    
+
+        # Aqui deixei só um exemplo, tentar formular pra ele criar as cartas automaticamente
+        self$cards <- matrix(c(
+            "azul", 7,
+            "vermelho", 11,
+            "verde", 3,
+            "amarelo", 8
+        ), ncol = 2, byrow = TRUE) 
+
     }
+    
+    suffle = function() {
+
+      self$cards <- self$cards[sample(nrow(self$cards)), ]
+          
+    }
+
   )
+
 )
 
 deck <- Deck$new()
+
