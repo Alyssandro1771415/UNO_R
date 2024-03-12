dfb7d5cdbdcceb578a01b5762814a3051569d407
Alyssandro <dyogo.alyssandro@gmail.com>
2024-03-12 15:01:49 -0300

    feat(moving Cards class): Miving cards class for main branch too

---
+++ Cards.R
@@ -0,0 +1,23 @@
+library(R6)
+
+Cards <- R6Class("Cards",
+
+    public = list(
+
+        color = NULL,
+        number = NULL,
+
+        initialize = function(color = NA, number = NA) {
+         self$color <- color
+         self$number <- number
+        }
+
+
+
+
+
+
+
+
+    )
+)
